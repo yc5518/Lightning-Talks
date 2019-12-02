72df6dfa09f981d749ba8c39640b7cd8b51527e5
Chao Yang <chao@sphereidentity.com>
2019-12-02 22:41:03 +1300

    added google login backend

---
+++ lightning-talks-server/src/main/java/com/neighbourly/lightningtalks/security/CustomUserDetailsService.java
@@ -0,0 +1,41 @@
+package com.neighbourly.lightningtalks.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.neighbourly.lightningtalks.model.User;
+import com.neighbourly.lightningtalks.repository.UserRepository;
+import com.neighbourly.lightningtalks.security.exception.ResourceNotFoundException;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email : " + email)
+        );
+
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("User", "id", id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
